c7d533d9e1c2fc85c22fc724bb7f74ade1f46d93
cejoven-deb <carlosvivax77@hotmail.com>
2022-01-30 23:16:18 -0500

    trabajando listacatalogo

--- app/src/main/java/com/example/regalosdepelos/AdaptadorCatalogo.java
+++ app/src/main/java/com/example/regalosdepelos/AdaptadorCatalogo.java
@@ -1,8 +1,16 @@
 package com.example.regalosdepelos;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 
 public class AdaptadorCatalogo extends RecyclerView.Adapter<AdaptadorCatalogo.ViewHolderCatalogos>{
+     ArrayList<Catalogo> listaCatalogos;
 
+     public AdaptadorCatalogo(ArrayList<Catalogo> listaCatalogos){
+         this.listaCatalogos = listaCatalogos;
+
+
+     }
 
 }
