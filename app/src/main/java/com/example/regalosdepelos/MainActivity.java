35ea2b8a7a360e281528a67eccfe809727d305e8
cejoven-deb <carlosvivax77@hotmail.com>
2022-01-31 00:35:29 -0500

    trabajando seriebusqueda

--- app/src/main/java/com/example/regalosdepelos/MainActivity.java
+++ app/src/main/java/com/example/regalosdepelos/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.regalosdepelos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    SearchView txtBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtBuscar = findViewById(R.id.txtBuscar);
     }
 }
