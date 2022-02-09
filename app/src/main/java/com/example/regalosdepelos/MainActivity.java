5e7d38027d711cb481bbb720afc43d0b47de0bb7
santivivas <santivivas@unicauca.edu.co>
2022-02-09 01:11:47 -0500

    Se agrega boton menu de la manera planteada en el documento

--- app/src/main/java/com/example/regalosdepelos/MainActivity.java
+++ app/src/main/java/com/example/regalosdepelos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.regalosdepelos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -9,18 +10,34 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.regalosdepelos.db.DbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageBotonMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageBotonMenu = findViewById(R.id.imageBotonMenu);
+
         DbHelper dbHelper = new DbHelper(MainActivity.this);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        imageBotonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, MenuPrincipal.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
